fa912fd61d6d6f069a02aa9ae3b76d80c1727828
cesartidrick <cesartidrick@gmail.com>
2022-05-07 14:54:23 -0600

    commit tree 501634ce22427dd89503fa81351220a485862fef

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener('DOMContentLoaded', function() {
+    const words = document.getElementById('text')
+    words.textContent = 'This is really cool!'
+})
+console.log('where am i?')
